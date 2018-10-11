686daf3480797139eb2e5eafcabfca0c64d35069
Jonathan ‘J5’ Cook <jonathan.j5.cook@gmail.com>
2018-10-11 17:11:46 -0400

    Updating to new theme / copy from other site

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // webpack folder’s entry js — excluded from jekll’s build process.
+  entry: "./webpack/entry.js",
+  output: {
+    // we’re going to put the generated file in the assets folder so jekyll will grab it.
+    // if using GitHub Pages, use the following:
+    // path: "assets/javascripts"
+    path: __dirname + "/assets/scripts/",
+    filename: "main.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: {
+          presets: ["react", "es2015"]
+        }
+      }
+    ]
+  }
+};
